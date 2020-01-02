a2baa95f5ec376fa1b98dd6733b2d06876952f1b
beviswong <ebeviswong@126.com>
2020-01-02 21:12:29 +0800

    add git clone.

--- main.cpp
+++ main.cpp
@@ -5,5 +5,6 @@ int main() {
     std::cout << "Come on, Bevis Wong!" << std::endl;
     std::cout << "Never give up!" << std::endl;
     std::cout << "the chosen one!" << std::endl;
+    std::cout << "git clone." << std::endl;
     return 0;
 }
