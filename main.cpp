b20be856b9e2569f1f531c662cee05791a89f276
beviswong <ebeviswong@126.com>
2020-01-02 20:56:33 +0800

    add never give up.

--- main.cpp
+++ main.cpp
@@ -3,5 +3,6 @@
 int main() {
     std::cout << "Hello, World!" << std::endl;
     std::cout << "Come on, Bevis Wong!" << std::endl;
+    std::cout << "Never give up!" << std::endl;
     return 0;
 }
