ee2f386f92351aa8874415a0e859c17435071d8c
beviswong <ebeviswong@126.com>
2020-01-02 21:06:39 +0800

    add the chosen one.

--- main.cpp
+++ main.cpp
@@ -4,5 +4,6 @@ int main() {
     std::cout << "Hello, World!" << std::endl;
     std::cout << "Come on, Bevis Wong!" << std::endl;
     std::cout << "Never give up!" << std::endl;
+    std::cout << "the chosen one!" << std::endl;
     return 0;
 }
